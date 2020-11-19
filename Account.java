e3dcafd7e229db8c9b77f0783a1eaa8bc3e93263
Ryan <47995762+Partyzra@users.noreply.github.com>
2020-11-18 21:38:13 -0700

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,23 @@
+public class Account {
+    String name;
+    int age;
+
+    public static void main(String[] args) {
+        Account a = new Account();
+
+        a.age = 34;
+        a.name = "Pablo";
+
+        System.out.println(a.age);
+        System.out.println(a.name);
+
+    }
+
+
+}
+
+
+/* A class represents objects in the real world, inside of a computer.
+
+
+ */
